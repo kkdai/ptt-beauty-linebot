32d94d05c42a00233d33d4fd3d12b5ea8d59b606
Evan Lin <evan.if.lin@linecorp.com>
2025-02-11 21:39:47 +0800

    ✨ Refactor article creation logic to improve error handling and code readability

--- controllers/controller.go
+++ controllers/controller.go
@@ -2,10 +2,10 @@ package controllers
 
 import (
 	"errors"
+	"fmt"
 	"sort"
 
 	"github.com/kkdai/favdb"
-
 	"github.com/kkdai/linebot-ptt-beauty/utils"
 	. "github.com/kkdai/photomgr"
 )
@@ -14,6 +14,32 @@ type UserFavorite struct {
 	Id        int64    `bson:"_id"`
 	UserId    string   `json:"user_id" bson:"user_id"`
 	Favorites []string `json:"favorites" bson:"favorites"`
+}
+
+// Helper to create an article document given an index.
+// Now checks error from GetAllFromURL (returns error string) and uses fallback values.
+func createArticle(ptt *PTT, index int) favdb.ArticleDocument {
+	title := ptt.GetPostTitleByIndex(index)
+	url := ptt.GetPostUrlByIndex(index)
+	post := favdb.ArticleDocument{
+		ArticleTitle: title,
+		URL:          url,
+		ArticleID:    utils.GetPttIDFromURL(url),
+	}
+	// Check for error from GetAllFromURL using empty-string check.
+	if errStr, images, push, boo := ptt.GetAllFromURL(url); errStr != "" {
+		// Fallback values if error (such as too many redirects)
+		post.ImageLinks = []string{}
+		post.MessageCount.Push = 0
+		post.MessageCount.Boo = 0
+		post.MessageCount.All = 0
+	} else {
+		post.ImageLinks = images
+		post.MessageCount.Push = push
+		post.MessageCount.Boo = boo
+		post.MessageCount.All = push + boo
+	}
+	return post
 }
 
 func GetOne(url string) (result *favdb.ArticleDocument, err error) {
@@ -35,21 +61,13 @@ func Get(page int, perPage int) (results []favdb.ArticleDocument, err error) {
 	var ret []favdb.ArticleDocument
 	ptt := NewPTT()
 	count := ptt.ParsePttPageByIndex(page, true)
+	fmt.Println("Total count:", count)
 	for i := 0; i < count && i < perPage; i++ {
 		title := ptt.GetPostTitleByIndex(i)
 		if utils.CheckTitleWithBeauty(title) {
-			post := favdb.ArticleDocument{}
-			url := ptt.GetPostUrlByIndex(i)
-			post.ArticleTitle = title
-			post.URL = url
-			post.ArticleID = utils.GetPttIDFromURL(url)
-			_, post.ImageLinks, post.MessageCount.Push, post.MessageCount.Boo = ptt.GetAllFromURL(url)
-			post.MessageCount.All = post.MessageCount.Push + post.MessageCount.Boo
-			post.MessageCount.Count = ptt.GetPostStarByIndex(i)
-			ret = append(ret, post)
+			ret = append(ret, createArticle(ptt, i))
 		}
 	}
-
 	return ret, nil
 }
 
@@ -62,22 +80,15 @@ func GetRandom(count int) (results []favdb.ArticleDocument, err error) {
 	}
 	var ret []favdb.ArticleDocument
 	for i := 0; i < count; i++ {
-		title := ptt.GetPostTitleByIndex(rands[i])
-		post := favdb.ArticleDocument{}
-		url := ptt.GetPostUrlByIndex(rands[i])
-		post.ArticleTitle = title
-		post.URL = url
-		post.ArticleID = utils.GetPttIDFromURL(url)
-		_, post.ImageLinks, post.MessageCount.Push, post.MessageCount.Boo = ptt.GetAllFromURL(url)
-		post.MessageCount.All = post.MessageCount.Push + post.MessageCount.Boo
-		post.MessageCount.Count = ptt.GetPostStarByIndex(i)
+		index := rands[i]  // use the random index
+		post := createArticle(ptt, index)
+		post.MessageCount.Count = ptt.GetPostStarByIndex(index)
 		ret = append(ret, post)
 	}
 	return ret, nil
 }
 
 func GetKeyword(count int, keyword string) (results []favdb.ArticleDocument, err error) {
-
 	ptt := NewPTT()
 	pCount := ptt.ParseSearchByKeyword(keyword)
 	if pCount == 0 {
@@ -85,16 +96,7 @@ func GetKeyword(count int, keyword string) (results []favdb.ArticleDocument, err
 	}
 	var ret []favdb.ArticleDocument
 	for i := 0; i < count; i++ {
-		title := ptt.GetPostTitleByIndex(i)
-		post := favdb.ArticleDocument{}
-		url := ptt.GetPostUrlByIndex(i)
-		post.ArticleTitle = title
-		post.URL = url
-		post.ArticleID = utils.GetPttIDFromURL(url)
-		_, post.ImageLinks, post.MessageCount.Push, post.MessageCount.Boo = ptt.GetAllFromURL(url)
-		post.MessageCount.All = post.MessageCount.Push + post.MessageCount.Boo
-		post.MessageCount.Count = ptt.GetPostStarByIndex(i)
-		ret = append(ret, post)
+		ret = append(ret, createArticle(ptt, i))
 	}
 	return ret, nil
 }
@@ -109,26 +111,35 @@ func GetMostLike(total int, limit int) (results []favdb.ArticleDocument, err err
 	var ret []favdb.ArticleDocument
 	for i := 0; i < pCount; i++ {
 		title := ptt.GetPostTitleByIndex(i)
-		post := favdb.ArticleDocument{}
 		url := ptt.GetPostUrlByIndex(i)
-		post.ArticleTitle = title
-		post.URL = url
-		post.ArticleID = utils.GetPttIDFromURL(url)
-		post.MessageCount.Count = ptt.GetPostStarByIndex(i)
+		post := favdb.ArticleDocument{
+			ArticleTitle: title,
+			URL:          url,
+			ArticleID:    utils.GetPttIDFromURL(url),
+			MessageCount: favdb.MessageCount{
+				Count: ptt.GetPostStarByIndex(i),
+			},
+		}
 		ret = append(ret, post)
 	}
-	//Sort it.
+	// Sort articles by like count in descending order.
 	sort.Sort(favdb.AllArticles(ret))
-
-	//Get the first limit (10)
+	// Get the top limit entries.
 	ret = ret[0:limit]
-
-	//Add each images and like/dis to top 10.
-	for k, _ := range ret {
+	// Enrich each article with images and like details with error handling.
+	for k := range ret {
 		url := ret[k].URL
-		_, ret[k].ImageLinks, ret[k].MessageCount.Push, ret[k].MessageCount.Boo = ptt.GetAllFromURL(url)
-		ret[k].MessageCount.All = ret[k].MessageCount.Push + ret[k].MessageCount.Boo
+		if errStr, images, push, boo := ptt.GetAllFromURL(url); errStr != "" {
+			ret[k].ImageLinks = []string{}
+			ret[k].MessageCount.Push = 0
+			ret[k].MessageCount.Boo = 0
+			ret[k].MessageCount.All = 0
+		} else {
+			ret[k].ImageLinks = images
+			ret[k].MessageCount.Push = push
+			ret[k].MessageCount.Boo = boo
+			ret[k].MessageCount.All = push + boo
+		}
 	}
-
 	return ret, nil
 }
--- controllers/controller_test.go
+++ controllers/controller_test.go
@@ -5,28 +5,28 @@ import (
 )
 
 func TestGet(t *testing.T) {
-	if ret, err := Get(3, 5); nil != err {
+	if ret, err := Get(3, 5); err != nil {
 		t.Error(err)
 	} else {
 		if len(ret) < 5 {
-			t.Error("Can not get result")
+			t.Error("Cannot get result")
 		}
 	}
 }
 
 func TestGetMostLike(t *testing.T) {
-	if ret, err := GetMostLike(6, 5); nil != err {
+	if ret, err := GetMostLike(6, 5); err != nil {
 		t.Error(err)
 	} else {
 		if len(ret) != 5 {
-			t.Error("Can not get enough result:", len(ret))
+			t.Error("Cannot get enough result:", len(ret))
 		}
 		for k, v := range ret {
 			if k == 0 {
 				continue
 			}
 			if ret[k-1].MessageCount.Count < v.MessageCount.Count {
-				t.Error("cannot get most like list k-1=", ret[k-1].MessageCount.Count, " v=,", v.MessageCount.Count)
+				t.Error("Most like list is not in order: previous=", ret[k-1].MessageCount.Count, " current=", v.MessageCount.Count)
 			}
 		}
 	}
